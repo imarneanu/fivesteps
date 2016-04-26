266665fed3efb6a8109f415fa6f9de7bd4492a86
imarneanu <iulia.marneanu@appsrise.com>
2016-04-26 12:04:20 +0300

    First design draft

--- app/src/main/java/com/imarneanu/fivesteps/MainActivity.java
+++ app/src/main/java/com/imarneanu/fivesteps/MainActivity.java
@@ -2,6 +2,8 @@ package com.imarneanu.fivesteps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initiateViews();
+    }
+
+    private void initiateViews() {
+        Spinner spinner = (Spinner) findViewById(R.id.event_time);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.time_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
