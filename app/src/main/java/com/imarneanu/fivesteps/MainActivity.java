8c9e174a2d211d9889205435a6e626179528dc37
imarneanu <iulia.marneanu@appsrise.com>
2016-04-28 16:36:04 +0300

    Pick location from Google Maps

--- app/src/main/java/com/imarneanu/fivesteps/MainActivity.java
+++ app/src/main/java/com/imarneanu/fivesteps/MainActivity.java
@@ -1,5 +1,6 @@
 package com.imarneanu.fivesteps;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
@@ -7,7 +8,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -15,27 +15,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 public class MainActivity extends AppCompatActivity {
 
     private final int SELECT_PHOTO = 1;
+    private final int SELECT_LOCATION = 2;
 
     private EditText mEventDate;
     private Spinner mEventTime;
+    private TextView mEventLocation;
+    private Button mPickLocation;
     private RelativeLayout mEventPoster;
     private Button mChangePoster;
 
     private int mScreenWidth;
 
+    private Activity mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
 
         initView();
         setupView();
@@ -44,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         mEventDate = (EditText) findViewById(R.id.event_date);
         mEventTime = (Spinner) findViewById(R.id.event_time);
+        mEventLocation = (TextView) findViewById(R.id.event_location);
+        mPickLocation = (Button) findViewById(R.id.pick_location);
         mEventPoster = (RelativeLayout) findViewById(R.id.event_poster);
         mChangePoster = (Button) findViewById(R.id.change_poster);
 
@@ -51,8 +65,6 @@ public class MainActivity extends AppCompatActivity {
         Point size = new Point();
         display.getSize(size);
         mScreenWidth = size.x;
-        Log.i("MainActivity", "displayWidth  = " + size.x);
-        Log.i("MainActivity", "displayHeight  = " + size.y);
     }
 
     private void setupView() {
@@ -64,6 +76,18 @@ public class MainActivity extends AppCompatActivity {
                 R.array.time_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mEventTime.setAdapter(adapter);
+
+        mPickLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                    startActivityForResult(builder.build(mContext), SELECT_LOCATION);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         mChangePoster.setOnClickListener(new View.OnClickListener() {
 
@@ -77,19 +101,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
             case SELECT_PHOTO:
                 if (resultCode == RESULT_OK) {
-                    Uri selectedImage = imageReturnedIntent.getData();
+                    Uri selectedImage = data.getData();
                     try {
                         mEventPoster.setBackground(decodeUri(selectedImage));
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
                 }
+                break;
+            case SELECT_LOCATION:
+                if (resultCode == RESULT_OK) {
+                    Place place = PlacePicker.getPlace(data, this);
+                    String location = place.getName() + "\n" + place.getAddress();
+                    mEventLocation.setText(location);
+                    mEventLocation.setVisibility(View.VISIBLE);
+                }
+                break;
         }
     }
 
